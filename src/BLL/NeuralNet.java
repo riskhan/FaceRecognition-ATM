cfb600391705fe71e7eb8a32f87a0dfe9e83b757
User <User@192.168.1.5>
2018-03-23 01:17:37 +0530

    done with the training and recognizing part in the neural network complete the creating set

--- src/BLL/NeuralNet.java
+++ src/BLL/NeuralNet.java
@@ -13,7 +13,11 @@ import java.util.Arrays;
 import javax.imageio.ImageIO;
 import org.neuroph.core.*;
 import org.neuroph.core.data.DataSet;
+import org.neuroph.core.data.DataSetRow;
+import org.neuroph.core.data.norm.MaxMinNormalizer;
+import org.neuroph.core.data.norm.Normalizer;
 import org.neuroph.nnet.MultiLayerPerceptron;
+import org.neuroph.nnet.learning.MomentumBackpropagation;
 import org.neuroph.util.TransferFunctionType;
 /**
  *
@@ -26,9 +30,8 @@ import org.neuroph.util.TransferFunctionType;
  */
 public class NeuralNet {
     
-    File dir = new File(".\\trainingset");
-    
-    
+    static File dir = new File(".\\trainingset");
+  
     static final String[] extensions = new String[]{//the file extensions
         "gif", "png", "bmp","jpg"
     };
@@ -47,11 +50,12 @@ public class NeuralNet {
     };
     
     /**
-    * @param outputSize the size of the output(the number of customers)
-    */
-    public void createNetwork(int outputSize)//create the neural network
+     * This method creates the training set
+     * @param outputSize the size of the output(the number of customers)
+     */
+    public static void createTrainingset(int outputSize)
     {
-        String imgName="",imgId="";
+        String imgName,imgId;
         int id,x;
         DataSet trainingSet=new DataSet(80, outputSize);//initialize the dataset
         BufferedImage img=null;
@@ -63,14 +67,17 @@ public class NeuralNet {
                 {
                     img=ImageIO.read(f);
                     imgName=f.getName();//get the name of the image
-                    imgId=imgName.substring(0, 1);//get the id of the image
+                    imgId=imgName.substring(0, 1);//get the id of the image by getting the first letter of the image
                     id=Integer.parseInt(imgId);
+                    //to create the output array
                     double[]output=new double[outputSize];
                     Arrays.fill(output,0);
-                    output[id-1]=1;//create the output of the inputs
-                    
-                    
+                    output[id-1]=1;
+                    //to create the input array
+                    double []input=new double[80];
                     /*do the gabor filtering and extract the features and put to dataset*/
+                    //to add new input row to the network
+                    trainingSet.addRow(new DataSetRow(input,new double[]{2,35,4,5,6}));
                 }
                 catch(IOException e)
                 {
@@ -78,15 +85,65 @@ public class NeuralNet {
                 }
             }
         }
+        Normalizer norm=new MaxMinNormalizer();
+        norm.normalize(trainingSet);
+        trainingSet.saveAsTxt("trainingdataSet.txt",",");
+        
     }
     
-    public double[] recognizeFaces(double[] testImage)
+    
+    public void trainNetwork(int outputSize,int hidden,double lrate,double momentum)
     {
-        NeuralNetwork neural=NeuralNetwork.load(".\\neuralNet");
+        File trainingSet=new File(".\\trainingset\\trainingdataSet.txt");
+        DataSet training = null;
+        MultiLayerPerceptron neuralNet = new MultiLayerPerceptron(TransferFunctionType.SIGMOID,80,hidden ,outputSize);
+        MomentumBackpropagation backProp = new MomentumBackpropagation() ;
+        backProp.setLearningRate(lrate);
+        backProp.setMomentum(momentum);
+        backProp.setMaxError(0.001);
+        backProp.setMaxIterations(10000);
+        
+        if(trainingSet.exists())
+        {
+            training=DataSet.load(".\\trainingset\\trainingdataSet.txt");
+        }
+        else
+        {
+            createTrainingset(outputSize);
+        }
+        neuralNet.setLearningRule(backProp);
+        neuralNet.learn(training);
+    }
+    
+    /**
+     * Method to recognize input faces
+     * @param testImage the features of the image to be recognized
+     * @returns the highest output of the neural network
+     */
+    public int recognizeFaces(double[] testImage)
+    {
+        NeuralNetwork neural=NeuralNetwork.load(".\\faceRec.nnet");
         neural.setInput(testImage);
         neural.calculate();
-        double[] output=neural.getOutput();
-        return output;
+        double[] output=neural.getOutput();//get the output of the neural network
+        
+        //method to get the highest id from the output 
+        int high=0,id;
+        for(int i=1;i<output.length;i++)
+        {
+            if(output[i]>=output[high])
+            {
+                high=i;
+            }     
+        }
+        id=high+1;
+        return id;
+    }
+    
+    
+    public static void main(String args[])
+    {
+        createTrainingset(5);
     }
 
 }
